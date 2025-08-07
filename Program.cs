8364fca11e028cf63f5ab922b0ccaf47ce8f54d3
khoavu1831 <khoavu1831@gmail.com>
2025-08-08 00:12:39 +0700

    updt

--- Program.cs
+++ Program.cs
@@ -3,7 +3,40 @@
     public static void Main(string[] args)
     {
         HandleFunctions handleFunctions = new HandleFunctions();
-        handleFunctions.PrintRandomSalaries();
+
+        // int result = handleFunctions.GetSalaries(handleFunctions.RandomSalariesString());
+        // Console.Write(result);
+
+        // string rs = handleFunctions.RandomSalariesString();
+        string s = @"+1/1:141,2
+                    +2/1:19     
+                    +3/1:10,199 
+                    +4/1:118    
+                    +5/1:105,105
+                    +6/1:50,100,97
+                    +7/1:88,163
+                    +8/1:48,100
+                    +9/1:14
+                    +10/1:154,128
+                    +11/1:
+                    +12/1:
+                    +13/1:10
+                    +14/1:102
+                    +15/1:
+                    +16/1:137,125
+                    +17/1:36
+                    +18/1:166
+                    +19/1:23,55
+                    +20/1:4,147,37";
+        // int rs = handleFunctions.GetSalaries(s);
+        // int rs = handleFunctions.TotalSalaries(s);
+        int rs = handleFunctions.GetSalaries(s);
+        Console.Write(rs);
+        int rs2 = handleFunctions.GetSalaries(handleFunctions.RandomGenSalariesString(100, 2, 4));
+        string s2 =handleFunctions.RandomGenSalariesString(100, 2, 4);
+        Console.WriteLine(s2);
+        Console.Write(rs2);
+
     }
 }
 
@@ -32,14 +65,12 @@ class HandleFunctions
 
         return 30;
     }
-
-    public void PrintRandomSalaries()
+    public string RandomGenSalariesString(int daysCount, int startDay, int startMonth)
     {
         int i = 0;
         string s = "";
-        int daysCount = 20;
-        int day = 1;
-        int month = 1;
+        int day = startDay;
+        int month = startMonth;
         int maxDay;
 
         while (i < daysCount)
@@ -52,7 +83,6 @@ class HandleFunctions
             {
                 maxDay = CheckEvenMonth(month);
             }
-
 
             if (day == maxDay + 1)
             {
@@ -68,7 +98,57 @@ class HandleFunctions
             day++;
             i++;
         }
-        Console.Write(s);
+        return s;
     }
 
+    public int TotalSalaries(string s)
+    {
+        int totalOfSalaries = 0;
+        int i = 0;
+        List<int> salaries = new List<int>();
+
+        while (i < s.Length)
+        {
+            if (s[i + 1] == '+')
+            {
+                i++;
+            }
+            if (s[i] == ':')
+            {
+                string salary = "";
+
+                while (i < s.Length && s[i] != '+')
+                {
+                    if (s[i] >= '0' && s[i] <= '9')
+                    {
+                        salary += s[i];
+                    }
+                    else if (s[i] == ',')
+                    {
+                        salaries.Add(Convert.ToInt32(salary));
+                        salary = "";
+                    }
+
+                    i++;
+                }
+                salaries.Add(Convert.ToInt32(salary));
+            }
+            i++;
+        }
+
+        foreach (var salary in salaries)
+        {
+            totalOfSalaries += salary;
+        }
+
+        return totalOfSalaries;
+    }
+
+    public int GetSalaries(string s)
+    {
+        string raw = s;
+        string input = raw.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+
+        return TotalSalaries(input);
+    }
 }
